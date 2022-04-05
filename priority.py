27a82e15680c72a2ad3d0bc5f9f54e82f88f2b18
Vortexx2 <janmayshuklay@gmail.com>
2022-04-05 14:33:02 +0530

    Fixed non preemp priority

--- priority.py
+++ priority.py
@@ -19,7 +19,39 @@ def priority(processes: List[Process]) -> Gantt:
 
       if not proc.isComplete():
         if proc.arrTime <= time:
-          
+
+          if not nextExec:
+            nextExec = proc
+
+          if proc.prio < nextExec.prio:
+            nextExec = proc
+
+    nextExecSub = SubProcess(time, nextExec.burst, nextExec)
+    nextExecSub.execute(gantt)
+    time += nextExec.burst
+    totalBurst -= nextExec.burst
+
+  return gantt
+
+
+def preemp_priority(processes: List[Process]) -> Gantt:
+  gantt = Gantt()
+
+  if not len(processes):
+    return gantt
+
+  totalBurst = total_burst(processes)
+  time = 0
+
+  while totalBurst:
+    nextExec = None
+
+    for proc in processes:
+
+      if not proc.isComplete():
+
+        if proc.arrTime <= time:
+
           if not nextExec:
             nextExec = proc
 
